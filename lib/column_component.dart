6e65e58017048473f4325131bc97af1674e01819
erjanmx <erjanmx@gmail.com>
2018-03-14 00:52:32 +0300

    Add column component

---
+++ lib/column_component.dart
@@ -0,0 +1,13 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'emx-column',
+  templateUrl: 'column_component.html',
+  styleUrls: const ['column_component.css'],
+)
+
+class ColumnComponent implements OnInit  {
+
+  ngOnInit() {}
+
+}
