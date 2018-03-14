437437fb1df5cd58164168228b28c0e2ab3067e9
erjanmx <erjanmx@gmail.com>
2018-03-14 18:56:33 +0300

    Use card component

--- lib/src/column_component.dart
+++ lib/src/column_component.dart
@@ -1,12 +1,14 @@
 import 'package:angular/angular.dart';
 
+import 'package:slack_reports/src/card.dart';
 import 'package:slack_reports/src/column.dart';
+import 'package:slack_reports/src/card_component.dart';
 
 @Component(
   selector: 'emx-column',
   templateUrl: 'column_component.html',
   styleUrls: const ['column_component.css'],
-  directives: const [CORE_DIRECTIVES],
+  directives: const [CORE_DIRECTIVES, CardComponent],
 )
 
 
@@ -14,5 +16,17 @@ class ColumnComponent {
   @Input()
   Column column;
 
+  @Input()
+  List<Card> cards = [];
+
   bool addingCard = false;
+
+  void addCard(String title) {
+    this.cards.add(new Card(1, title, this.column.id, 1));
+    this.addingCard = false;
+  }
+
+  void enableAddCard() {
+    this.addingCard = true;
+  }
 }
