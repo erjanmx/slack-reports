19f5cb63cd9e98ae345efc4d586ac2f3e91b1e97
erjanmx <erjanmx@gmail.com>
2018-03-14 18:56:22 +0300

    Add card component

---
+++ lib/src/card.dart
@@ -0,0 +1,10 @@
+
+class Card {
+  final int id;
+
+  String title;
+  int columnId;
+  int order;
+
+  Card(this.id, this.title, this.columnId, this.order);
+}
---
+++ lib/src/card_component.dart
@@ -0,0 +1,17 @@
+import 'package:angular/angular.dart';
+
+import 'package:slack_reports/src/card.dart';
+
+@Component(
+  selector: 'emx-card',
+  templateUrl: 'card_component.html',
+  directives: const [CORE_DIRECTIVES],
+)
+
+
+class CardComponent {
+  @Input()
+  Card card;
+
+  bool editingCard = false;
+}
