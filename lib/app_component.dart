878e9fc37d40235bdb011354d2197943cd5e27ef
erjanmx <erjanmx@gmail.com>
2018-03-14 17:54:15 +0300

    Move components in child folder

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,17 +1,21 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
 
-//import 'package:slack_reports/column_component.dart';
+import 'package:slack_reports/src/column.dart';
+import 'package:slack_reports/src/column_component.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: const ['app_component.css'],
-  directives: const [CORE_DIRECTIVES, formDirectives],
+  directives: const [CORE_DIRECTIVES, ColumnComponent],
 )
 
 class AppComponent {
   final title = 'Dashboard';
 
-  final List columns = ['Todo', 'Doing', 'Done'];
+  final List<Column> columns = [
+    new Column(1, 'Todo'),
+    new Column(2, 'Doing'),
+    new Column(3, 'Done'),
+  ];
 }
--- lib/src/column_component.dart
+++ lib/src/column_component.dart
@@ -1,13 +1,18 @@
 import 'package:angular/angular.dart';
 
+import 'package:slack_reports/src/column.dart';
+
 @Component(
   selector: 'emx-column',
   templateUrl: 'column_component.html',
   styleUrls: const ['column_component.css'],
+  directives: const [CORE_DIRECTIVES],
 )
 
-class ColumnComponent implements OnInit  {
 
-  ngOnInit() {}
+class ColumnComponent {
+  @Input()
+  Column column;
 
+  bool addingCard = false;
 }
