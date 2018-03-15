c28fc7d88ba63a3f3356fa5909ce935ed5faec51
erjanmx <erjanmx@gmail.com>
2018-03-15 14:10:05 +0300

    Use board model

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,5 +1,7 @@
 import 'package:angular/angular.dart';
 
+import 'package:slack_reports/src/board.dart';
+import 'package:slack_reports/src/card.dart';
 import 'package:slack_reports/src/column.dart';
 import 'package:slack_reports/src/column_component.dart';
 
@@ -13,9 +15,21 @@ import 'package:slack_reports/src/column_component.dart';
 class AppComponent {
   final title = 'Dashboard';
 
-  final List<Column> columns = [
-    new Column(1, 'Todo'),
-    new Column(2, 'Doing'),
-    new Column(3, 'Done'),
-  ];
+  Board board = new Board();
+
+  AppComponent() {
+    this.board.columns = [
+      new Column(1, 'Todo'),
+      new Column(2, 'Doing'),
+      new Column(3, 'Done'),
+    ];
+
+    this.board.cards = [
+      new Card(1, 'Test-1', 1, 1),
+      new Card(2, 'Test-2', 2, 1),
+      new Card(3, 'Test-3', 2, 1),
+      new Card(4, 'Test-4', 2, 1),
+      new Card(5, 'Test-5', 3, 1),
+    ];
+  }
 }
