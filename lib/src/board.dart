79d515eecd58a64d90676792a1eff6e37cd5e915
erjanmx <erjanmx@gmail.com>
2018-03-15 14:29:15 +0300

    Add output textarea

--- lib/src/board.dart
+++ lib/src/board.dart
@@ -4,4 +4,20 @@ import 'column.dart';
 class Board {
   List<Column> columns = [];
   List<Card> cards = [];
+
+  @override
+  String toString() {
+    String output = '';
+    this.columns.forEach((Column column) {
+      output += column.name + '\n';
+
+      this.cards.where((Card card) => (card.columnId == column.id)).toList().forEach((Card card) {
+        output += card.title + '\n';
+      });
+
+      output += '\n';
+    });
+
+    return output;
+  }
 }
