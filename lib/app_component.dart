dcf8af7b5fa98d03317b59da7e113f32854f6345
erjanmx <erjanmx@gmail.com>
2018-03-16 00:06:27 +0300

    Attaching projectId

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -29,20 +29,21 @@ class AppComponent {
     this.board.cards = [
       new Card(1, 'Test-1', 1, 0, 1),
       new Card(2, 'Test-2', 1, 0, 1),
-      new Card(3, 'Test-3', 1, 1, 1),
-      new Card(4, 'Test-4', 1, 2, 1),
-      new Card(5, 'Test-5', 2, 0, 2),
-      new Card(6, 'Test-6', 2, 0, 0),
-      new Card(7, 'Test-6', 2, 0, 0),
-      new Card(8, 'Test-6', 2, 0, 0),
-      new Card(9, 'Test-6', 2, 0, 0),
-      new Card(10, 'Test-6', 3, 1, 0),
-      new Card(11, 'Test-6', 3, 2, 0),
-      new Card(12, 'Test-6', 3, 1, 0),
-      new Card(13, 'Test-6', 3, 1, 0),
+      new Card(3, 'Test-3', 2, 2, 1),
+      new Card(4, 'Test-4', 2, 2, 1),
+      new Card(5, 'Test-5', 3, 0, 1),
+      new Card(6, 'Test-6', 3, 0, 0),
+      new Card(7, 'Test-7', 2, 0, 0),
+      new Card(8, 'Test-8', 2, 0, 0),
+      new Card(9, 'Test-9', 2, 0, 0),
+      new Card(10, 'Test-10', 3, 1, 0),
+      new Card(11, 'Test-11', 1, 2, 0),
+      new Card(12, 'Test-12', 2, 1, 0),
+      new Card(13, 'Test-13', 3, 1, 0),
     ];
 
     this.board.projects = [
+      new Card(0, '-', 0, 0, 0),
       new Card(1, 'Project-1', 0, 1, 0),
       new Card(2, 'Project-2', 0, 2, 1),
     ];
@@ -64,5 +65,14 @@ class AppComponent {
     } else {
       this.board.cards.add(card);
     }
+  }
+
+  void attachProject(Card card) {
+    this.board.cards = this.board.cards.map((Card c) {
+      if (c.id == card.id) {
+        c.projectId = card.projectId;
+      }
+      return c;
+    }).toList();
   }
 }
