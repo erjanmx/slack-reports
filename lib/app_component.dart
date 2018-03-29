d6656c3f146ea80213149dad6c250aa3f99aa37d
erjanmx <erjanmx@gmail.com>
2018-03-29 14:49:21 +0300

    Rename events

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,16 +1,14 @@
 import 'dart:html';
 import 'dart:async';
-import 'package:angular/angular.dart';
 
-import 'package:slack_reports/src/uuid.dart';
 import 'package:slack_reports/src/board.dart';
-import 'package:slack_reports/src/card.dart';
-import 'package:slack_reports/src/column.dart';
-import 'package:slack_reports/src/column_component.dart';
-import 'package:angular_components/angular_components.dart';
+import 'package:slack_reports/src/card/card.dart';
+import 'package:slack_reports/src/column/column_component.dart';
 
-import 'dart:convert' show JSON;
 import 'package:slack_reports/src/lib/assortment.dart';
+
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 
 @Component(
   selector: 'my-app',
@@ -20,98 +18,37 @@ import 'package:slack_reports/src/lib/assortment.dart';
 )
 
 class AppComponent {
-  final title = 'Dashboard';
+  Board board;
 
+  final String title = 'Dashboard';
   final String version = '0.1.0';
 
-  Board board = new Board();
+  AppComponent() {
+    board = new Board('${this.title}-${this.version}');
 
-  void initLocalStorage() {
-    var jsonList = window.localStorage['slack-reports-${this.version}'];
-    try {
-      Map<String, List> board = JSON.decode(jsonList);
-
-      this.board.cards = board['cards'].map((item) => new Card.fromJson(item)).toList();
-      this.board.projects = board['projects'].map((item) => new Card.fromJson(item)).toList();
-    } catch (e) {
-      print(e);
-      this.board.cards = [
-        new Card(uuid(), 'Task-1', 1, 1, ''),
-        new Card(uuid(), 'Task-2', 1, 0, ''),
-        new Card(uuid(), 'Task-3', 2, 0, ''),
-        new Card(uuid(), 'Task-4', 2, 1, ''),
-        new Card(uuid(), 'Task-5', 3, 0, ''),
-      ];
-
-      this.board.projects = [
-        new Card('', '-', 0, 0, ''),
-        new Card(uuid(), 'Project-1', 0, 1, ''),
-        new Card(uuid(), 'Project-2', 0, 2, ''),
-        new Card(uuid(), 'Project-3', 0, 3, ''),
-      ];
-    }
+    initializeBoard();
   }
 
-  void save() {
-    Map<String, List<Card>> list = {
-      'cards': this.board.cards,
-      'projects': this.board.projects,
-    };
+  void initializeBoard() {
+    this.board.load();
 
-    window.localStorage['slack-reports-${this.version}'] = JSON.encode(list);
-  }
-
-  void reload([bool s=true]) {
-    if (s) save();
-
-    initLocalStorage();
     new Future.delayed(const Duration(milliseconds: 500), () => setupView());
   }
 
-  AppComponent() {
-    this.board.columns = [
-      new Column(1, 'Todo'),
-      new Column(2, 'Doing'),
-      new Column(3, 'Done'),
-    ];
+  void reload() {
+    this.board.save();
 
-    this.reload(false);
+    initializeBoard();
   }
 
-  deleteProject(Card card) {
-    if (card.columnId == 0) {
-      this.board.cards = this.board.cards.map((Card c) {
-        if (c.projectId == card.id) {
-          c.projectId = '';
-        }
-        return c;
-      }).toList();
-
-      this.board.projects.remove(card);
-    }
-
-    this.board.cards.remove(card);
+  deleteCard(Card card) {
+    this.board.delete(card);
 
     this.reload();
   }
 
   void addCard(Card card) {
-    if (card.columnId == 0) {
-      this.board.projects.add(card);
-    } else {
-      this.board.cards.add(card);
-    }
-
-    this.reload();
-  }
-
-  void attachProject(Card card) {
-    this.board.cards = this.board.cards.map((Card c) {
-      if (c.id == card.id) {
-        c.projectId = card.projectId;
-      }
-      return c;
-    }).toList();
+    this.board.add(card);
 
     this.reload();
   }
@@ -150,20 +87,11 @@ class AppComponent {
       List<Element> elements = querySelectorAll('.project-card');
 
       int i = 0;
-      int c_id = 0;
       Card card;
       for (Element element in elements) {
         String card_id = element.getAttribute('data-card-id');
-        int column_id = int.parse(element.parent.getAttribute('data-column-id'));
-
         card = board.getProjectById(card_id);
-
-        if (c_id != column_id) i = 0;
-
-        card.columnId = column_id;
         card.order = i++;
-
-        c_id = column_id;
       };
 
       this.reload();
