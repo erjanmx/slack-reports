3744f480151a88865b496279a4bc86ac04734064
erjanmx <erjanmx@gmail.com>
2018-03-29 14:47:15 +0300

    Move cards

--- lib/src/card/card.dart
+++ lib/src/card/card.dart
@@ -1,3 +1,4 @@
+import 'package:slack_reports/src/lib/uuid.dart';
 
 class Card {
   String id;
@@ -27,5 +28,19 @@ class Card {
     order = json['order'];
     columnId = json['columnId'];
     projectId = json['projectId'];
+  }
+
+  Card.fromForm(String _title, int _columnId) {
+    id = uuid();
+    title = _title;
+    columnId = _columnId;
+    order = 999;
+    projectId = '';
+  }
+
+  bool isProject() => this.columnId == 0;
+
+  void unsetProject() {
+    this.projectId = '';
   }
 }
